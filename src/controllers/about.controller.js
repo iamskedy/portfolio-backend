7a06fa65076135cb98450270a28ed356c283e274
Shubham Dubey <shubhamkumar.dubey02@gmail.com>
2025-11-16 19:16:50 +0530

    feat(about): implemented full About CRUD APIs with model, routes, controller and error handling

--- src/controllers/about.controller.js
+++ src/controllers/about.controller.js
@@ -1,5 +1,50 @@
-exports.getAll = (req, res) => res.send("getAll");
-exports.getOne = (req, res) => res.send("getOne");
-exports.create = (req, res) => res.send("create");
-exports.update = (req, res) => res.send("update");
-exports.remove = (req, res) => res.send("remove");
+const AboutService = require("../services/about.service");
+
+exports.getAll = async (req, res) => {
+  try {
+    const data = await AboutService.getAll();
+    res.json(data);
+  } catch (err) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+exports.getOne = async (req, res) => {
+  try {
+    const data = await AboutService.getOne(req.params.id);
+    res.json(data);
+  } catch (err) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+exports.create = async (req, res) => {
+  try {
+    console.log("BODY RECEIVED:", req.body);  
+
+    const about = await AboutService.create(req.body);
+    return res.status(201).json(about);
+  } catch (err) {
+    console.error("ABOUT CREATE ERROR:", err); 
+    return res.status(500).json({ message: "Server error" });
+  }
+};
+
+
+exports.update = async (req, res) => {
+  try {
+    const data = await AboutService.update(req.params.id, req.body);
+    res.json(data);
+  } catch (err) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+exports.remove = async (req, res) => {
+  try {
+    const data = await AboutService.remove(req.params.id);
+    res.json({ message: "Deleted", data });
+  } catch (err) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
